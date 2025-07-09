12a4ce1de146e9f1774835a0a5846bc387001a64
SotnikovSS <sotnikovss90@gmail.com>
2025-07-09 23:19:20 +0200

    go

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,46 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import css from './App.module.css'
-
+import { useState, useEffect } from "react";
+import css from "./App.module.css"
+import { getMovie } from "../../components/services/movie";
+import SearchBar from "../SearchBar/SearchBar";
+import type { Movie } from "../../types/movie";
+import toast, { Toaster } from "react-hot-toast";
+import Loader from "../../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieGrid from "../MovieGrid/MovieGrid";
 
 function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
+  const handleSubmit = async (query: string) => {
+    try {
+      setIsLoading(true);
+      setError(false);
+      setMovies([]);
+      const fetchedMovies = await getMovie(query);
+      if(!fetchedMovies.length){
+        toast.error("No movies found for your request.")
+        return
+      }
+      setMovies(fetchedMovies)
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
+  const onSelect = ()=>{};
   return (
-    <>
-     
-    </>
-  )
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSubmit} />
+      {isLoading && <Loader/>}
+      {error && <ErrorMessage/>}
+      {movies.length > 0 && <MovieGrid movies ={movies} onSelect={onSelect}/>}
+      <Toaster/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/services/movie.ts
+++ src/components/services/movie.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
-export type {Movie} from "../../types/movie"
+import type { Movie } from "../../types/movie";
 
 interface MoviesHttpResponse {
-    movies: Movie[]
+  results: Movie[];
 }
 
+const myKey = import.meta.env.VITE_API_KEY;
 
-export const getMovie = async (query: string):Promise<Movie[]> => {
-    const response = await axios.get<MoviesHttpResponse>()
-}
+export const getMovie = async (query: string): Promise<Movie[]> => {
+
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        `Bearer ${myKey}`,
+    },
+  };
+
+  const response = await axios.get<MoviesHttpResponse>(url, options);
+  return response.data.results;
+};
+
+
